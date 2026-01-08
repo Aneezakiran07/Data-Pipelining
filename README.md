56bed5ce650c0d1a7560b345ce2f0ad53fc54746
Aneeza Kiran <aneezakiran07@gmail.com>
2026-01-08 23:10:07 +0500

    made streamlit gui for 3 functions , drop dup rows/cols, and removing the trailing spaces

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Data-Pipelining
-This project is a data cleaning pipeline built using Streamlit and Python. The pipeline allows users to upload their files, and the system will clean the data and provide the cleaned data as output. The pipeline includes various data cleaning techniques
+# Data Cleaning Pipeline
+
+This project is a data cleaning pipeline built using Streamlit and Python. The pipeline allows users to upload their files, and the system will clean the data and provide the cleaned data as output. The pipeline includes various data cleaning techniques:
+   - Removed whitespace
+   - Drop Duplicates Rows
+   - Drop Duplicates Column
--- app.py
+++ app.py
@@ -10,46 +10,9 @@ st.set_page_config(
     page_icon="🧹",
     layout="wide"
 )
-
-st.title("Data Cleaning Pipeline")
-st.markdown(
-    "Upload a csv file to clean it using the pipeline."
-)
-
-#this divider creates a horizontal line in the app
-st.divider()
-
-st.subheader("Upload your data file")
-#uploading file section
-uploaded_file = st.file_uploader(
-    "Choose a CSV file",
-    type=['csv'],
-    help="Upload a CSV file containing missing values etc"
-)
-
-if uploaded_file is not None:
-
-    st.success("File uploaded successfully!")
-
-    try:
-        df = pd.read_csv(uploaded_file)
-
-        st.info(f"DataFrame loaded with {df.shape[0]} rows and {df.shape[1]} columns")
-
-        st.subheader("Data Preview")
-        st.dataframe(df.head(10), use_container_width=True)
-
-
-    except Exception as e:
-        st.error(f"Error reading the file: {e}")
-    # Here, you can add more steps of the data cleaning pipeline
-else:
-    st.warning("Please upload a file to proceed")
-
-
-
+#python functions for data cleaning
 def checking_valid_input(
-        pd:pd.DataFrame
+        df:pd.DataFrame
 ):
     if not isinstance(df,pd.DataFrame):
         raise TypeError("Input must be a pandas DataFrame")
@@ -78,9 +41,93 @@ def stripping_whitespace(
     return df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
     
 
-def fix_column_types(
-        df:pd.DataFrame
+def stripping_character_user_wants(
+        df:pd.DataFrame,
+        colname:str ,# tells us column name 
+        ch_toremove:str #s2 tells us character to remove
 ):
     checking_valid_input(df)
+    if colname in df.columns:
+        # regex=False tells pandas "this is just a normal symbol, not a complex code"
+        df[colname] = df[colname].astype(str).str.replace(ch_toremove, "", regex=False)
+    
+    return df
+
+#streamlit logic
+st.title("Data Cleaning Pipeline")
+st.markdown(
+    "Upload a csv file to clean it using the pipeline."
+)
+
+#this divider creates a horizontal line in the app
+st.divider()
+
+st.subheader("Upload your data file")
+#uploading file section
+uploaded_file = st.file_uploader(
+    "Choose a CSV file",
+    type=['csv'],
+    help="Upload a CSV file containing missing values etc"
+)
+
+if uploaded_file is not None:
+
+    st.success("File uploaded successfully!")
+
+    try:
+        df = pd.read_csv(uploaded_file)
+
+        st.info(f"DataFrame loaded with {df.shape[0]} rows and {df.shape[1]} columns")
+         # Store original df in session state for comparison
+        if 'original_df' not in st.session_state:
+            st.session_state.original_df = df.copy()
+        
+        # Store current working df in session state
+        if 'current_df' not in st.session_state:
+            st.session_state.current_df = df.copy()
+
+        st.subheader("Data Preview")
+        st.dataframe(df.head(10), use_container_width=True)
+
+        st.divider()
+
+        st.subheader("Cleaning Data")
+        st.markdown("Choose the cleaning operations to perform on your data")
+
+          # Create columns for buttons
+        col1, col2, col3, col4, col5 = st.columns(5)
+
+        with col1:
+            if st.button("Removing trailing whitespaces", use_container_width=True):
+                try:
+                    #session state is used to persist data across all interactions
+                    st.session_state.current_df = stripping_whitespace(st.session_state.current_df)
+                    st.success("Whitespace stripped from text columns!")
+                except Exception as e:
+                    st.error(f"Error: {str(e)}")
+        with col2:
+            if st.button("Drop Duplicate Rows", use_container_width=True):#container width is used for making button full width of col
+                try:
+                    before_rows=st.session_state.current_df.shape[0] #shape gives us (rows,cols)
+                    st.session_state.current_df= drop_duplicate_rows(st.session_state.current_df)
+                    after_rows=st.session_state.current_df.shape[0]
+                    st.success(f"Dropped {before_rows-after_rows} duplicate rows!")
+                except Exception as e:
+                    st.error(f"Error: {str(e)}")
+        with col3:
+            if st.button("Drop Duplicate Columns", use_container_width=True):
+                try:
+                    before_cols=st.session_state.current_df.shape[1] #shape 1 for cols
+                    st.session_state.current_df= drop_duplicate_columns(st.session_state.current_df)
+                    after_cols=st.session_state.current_df.shape[1]
+                    st.success(f"Dropped {before_cols-after_cols} duplicate columns!")
+                except Exception as e:
+                    st.error(f"Error: {str(e)}")
+
+    except Exception as e:
+        st.error(f"Error reading the file: {e}")
+    # Here, you can add more steps of the data cleaning pipeline
+else:
+    st.warning("Please upload a file to proceed")
 
 
